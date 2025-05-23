1ea26ceeacd92f8c92755ad0b1b439bfb02f18de
Matthew Campbell <m.campbell92@gmail.com>
2025-05-23 12:51:21 +0200

    initial footer configuration

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="flex items-center justify-between bg-primary w-screen px-10 mt-12 h-32 rounded-t-full">
+        <div className="">
+          <h1 className="font-bold text-text-secondary px-10">ESL LOGO</h1>
+        </div>
+      </footer>
+    </>
+  );
+}
