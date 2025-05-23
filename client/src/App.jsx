c6c035d0e82dce25b2f78518e2968470f8d09872
Matthew Campbell <m.campbell92@gmail.com>
2025-05-23 12:51:09 +0200

    import footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <Footer />
     </>
   );
 }
