b267d6d2251394ad8fcb3a159e77766608e63eb9
Matthew Campbell <m.campbell92@gmail.com>
2025-05-23 10:56:59 +0200

    add aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@ui": resolve(__dirname, "../packages/ui"),
+      "@utils": resolve(__dirname, "../packages/utils"),
+    },
+  },
 });
