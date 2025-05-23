8ce5b6ec2966add8a4c584014dbebc7ea8ed4fd5
Matthew Campbell <m.campbell92@gmail.com>
2025-05-23 10:57:25 +0200

    move css import to main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import "./index.css";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Home />
     </>
   );
 }
