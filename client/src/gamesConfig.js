ed8a489350211820f1a64178d033db8d3953069a
Matthew Campbell <m.campbell92@gmail.com>
2025-05-27 17:50:44 +0200

    configure routes

---
+++ client/src/gamesConfig.js
@@ -0,0 +1,7 @@
+export const games = [
+  {
+    path: "./pages/games/word-puzzle-popper",
+    name: "Word Puzzle Popper",
+    component: () => import("./pages/games/word-puzzle-popper/components/Game"),
+  },
+];
