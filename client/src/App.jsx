43401bfba05c3a09c5650b4082806f797c0a9e0c
Matthew Campbell <m.campbell92@gmail.com>
2025-05-27 17:50:20 +0200

    initial routing config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { games } from "./gamesConfig";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
@@ -5,9 +8,19 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Suspense fallback={<div>Loading... </div>}>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            {games.map(({ path, component }) => {
+              const Game = React.lazy(component);
+              return <Route key={path} path={path} element={<Game />}></Route>;
+            })}
+          </Routes>
+        </Suspense>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
