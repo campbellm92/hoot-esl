2c1d618db374c183db6ad480b4c63e7f2a08ab97
Matthew Campbell <m.campbell92@gmail.com>
2025-05-27 11:28:59 +0200

    add auto rows to fix layout issue

--- client/src/sections/home/Games.jsx
+++ client/src/sections/home/Games.jsx
@@ -5,26 +5,28 @@ import testImg from "../../assets/test.png";
 export default function Games() {
   return (
     <>
-      <div className="flex justify-center w-10/12 gap-4">
-        <Card
-          variant="primaryOutline"
-          image={testImg}
-          title="This is a game"
-          content="This is a nice description of the game"
-          button={<Button>Play game</Button>}
-        />
-        <Card
-          title="This is a game"
-          content="This is a nice description of the game"
-        />
-        <Card
-          title="This is a game"
-          content="This is a nice description of the game"
-        />
-        <Card
-          title="This is a game"
-          content="This is a nice description of the game"
-        />
+      <div className="w-10/12 mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 auto-rows-fr gap-4">
+          <Card
+            variant="primaryOutline"
+            image={testImg}
+            title="This is a game"
+            content="This is a nice description of the game"
+            button={<Button>Play game</Button>}
+          />
+          <Card
+            title="This is a game"
+            content="This is a nice description of the game"
+          />
+          <Card
+            title="This is a game"
+            content="This is a nice description of the game"
+          />
+          <Card
+            title="This is a game"
+            content="This is a nice description of the game"
+          />
+        </div>
       </div>
     </>
   );
