8b3055cc6c5dfef50b88c4d848874a37ef3d4e3b
Matthew Campbell <m.campbell92@gmail.com>
2025-05-30 10:39:36 +0200

    replace test components with games obj map

--- client/src/sections/home/GamesGrid.jsx
+++ client/src/sections/home/GamesGrid.jsx
@@ -1,31 +1,27 @@
+import { Link } from "react-router-dom";
 import { Card } from "@ui";
 import { Button } from "@ui";
-import testImg from "../../assets/test.png";
+import { games } from "../../gamesConfig";
 
 export default function GamesGrid() {
   return (
     <>
       <div className="w-10/12 mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 auto-rows-fr gap-4">
-          <Card
-            variant="primaryOutline"
-            image={testImg}
-            title="This is a game"
-            content="This is a nice description of the game"
-            button={<Button>Play game</Button>}
-          />
-          <Card
-            title="This is a game"
-            content="This is a nice description of the game"
-          />
-          <Card
-            title="This is a game"
-            content="This is a nice description of the game"
-          />
-          <Card
-            title="This is a game"
-            content="This is a nice description of the game"
-          />
+          {games.map((game) => (
+            <Card
+              key={game.id}
+              variant="primaryOutline"
+              title={game.name}
+              content={game.description}
+              image={game.image}
+              button={
+                <Link to={game.link}>
+                  <Button>Play Game</Button>
+                </Link>
+              }
+            />
+          ))}
         </div>
       </div>
     </>
