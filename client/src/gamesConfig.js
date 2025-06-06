078c2fcd309bcb7bd832d8b8fc9b9965120d8401
Matthew Campbell <m.campbell92@gmail.com>
2025-06-06 12:48:48 +0200

    config new game

--- client/src/gamesConfig.js
+++ client/src/gamesConfig.js
@@ -1,4 +1,4 @@
-// import testImg from "./assets/test.png";
+import testImg from "./assets/test.png";
 import wordPuzzlePopperImg from "./assets/word-puzzle-popper.webp";
 
 export const games = [
@@ -11,5 +11,15 @@ export const games = [
     path: "./pages/games/word-puzzle-popper",
     component: () => import("./pages/games/word-puzzle-popper/components/Game"),
     link: "/games/word-puzzle-popper",
+  },
+  {
+    name: "Word Memory Game",
+    description:
+      "Test your understanding of various words with this memory game",
+    image: testImg,
+    languageFocus: "Vocabulary",
+    path: "./pages/games/word-memory-game",
+    component: () => import("./pages/games/word-memory-game/components/Game"),
+    link: "/games/word-memory-game",
   },
 ];
