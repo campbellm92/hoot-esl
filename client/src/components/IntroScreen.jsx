4cba12f7cf13976852e5228ca20ab647d476381d
Matthew Campbell <m.campbell92@gmail.com>
2025-06-06 12:30:01 +0200

    fix layout

--- client/src/components/IntroScreen.jsx
+++ client/src/components/IntroScreen.jsx
@@ -4,7 +4,7 @@ import { SlideInContent } from "@animations";
 export default function IntroScreen({ name, instructions, onStart }) {
   return (
     <SlideInContent distance={300} reverse={true}>
-      <div className="flex justify-center items-center text-center w-screen h-screen">
+      <div className="flex justify-center items-center text-center w-screen min-h-screen pb-96">
         <div className="w-1/2 h-auto p-4 rounded-md border-4 border-primary">
           <h1 className="text-3xl font-semibold mb-4">{name}</h1>
           <p className="text-xl mb-4">{instructions}</p>
