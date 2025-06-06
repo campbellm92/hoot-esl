d64b74078ace93cbfb1d0924afffcdd51418b4bc
Matthew Campbell <m.campbell92@gmail.com>
2025-06-06 12:30:23 +0200

    add rounded effect

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,15 @@
 export default function Footer() {
   return (
     <>
-      <footer className="flex items-center justify-between bg-primary w-screen px-10 mt-12 h-32 rounded-t-full">
-        <div className="">
-          <h1 className="font-bold text-text-secondary px-10">ESL LOGO</h1>
+      <footer className="flex items-center bg-primary w-screen mt-12 h-42 rounded-t-full">
+        <div className="flex flex-col justify-center items-center w-full">
+          <h1 className="font-bold text-text-secondary">ESL LOGO</h1>
+          <p className="font-light text-text-secondary">By Matthew Campbell</p>
+          <div className="flex gap-4 p-4 text-text-secondary [&>a]:hover:text-text-primary [&>a]:font-medium">
+            <a href="">Games</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+          </div>
         </div>
       </footer>
     </>
