44a9f86aaf855c8b51fde64c511910c5bf5e9674
Matthew Campbell <m.campbell92@gmail.com>
2025-06-06 12:29:39 +0200

    change img for game

--- client/src/gamesConfig.js
+++ client/src/gamesConfig.js
@@ -1,11 +1,12 @@
-import testImg from "./assets/test.png";
+// import testImg from "./assets/test.png";
+import wordPuzzlePopperImg from "./assets/word-puzzle-popper.webp";
 
 export const games = [
   {
     name: "Word Puzzle Popper",
     description:
       "Improve your knowledge of nouns, verbs and adjectives in this grouping game.",
-    image: testImg,
+    image: wordPuzzlePopperImg,
     languageFocus: "Parts of speech",
     path: "./pages/games/word-puzzle-popper",
     component: () => import("./pages/games/word-puzzle-popper/components/Game"),
