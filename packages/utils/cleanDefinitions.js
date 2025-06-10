c3760bfefd8679d51608b9fe9215ef02fb6768c7
Matthew Campbell <m.campbell92@gmail.com>
2025-06-10 19:07:42 +0200

    add semicolons if there are more than one defintions

--- packages/utils/cleanDefinitions.js
+++ packages/utils/cleanDefinitions.js
@@ -1,4 +1,9 @@
 // remove the markup tags in word definitions
 export default function cleanDefinition(text) {
-  return text.replace(/\{.*?\}/g, "").trim();
+  return text
+    .replace(/\{.*?\}/g, ";") // replace tags with semicolon
+    .replace(/;{2,}/g, ";") // collapse multiple semicolons
+    .replace(/\s*;\s*/g, "; ") // normalise spaces around semicolons
+    .replace(/^;\s*/, "") // remove leading semicolon
+    .trim();
 }
