9b8b6cabadf6c2792aad9f879d2b9c59a0a292d5
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 16:29:34 +0200

    begin work on about page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="w-full min-h-screen p-34">
+      <p className="text-text-primary">
+        Hey, I'm Matt, and I created Hoot ESL (English as a Second Language) for
+        English learners to practise and improve their vocabulary through
+        (hopefully) fun games.
+      </p>
+    </div>
+  );
+}
