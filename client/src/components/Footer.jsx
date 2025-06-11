0bf3aec65b4156535a387f6ae392bfe3ac379e25
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 11:00:05 +0200

    change site name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <footer className="flex items-center bg-primary w-screen mt-12 h-42 rounded-t-full">
         <div className="flex flex-col justify-center items-center w-full">
-          <h1 className="font-bold text-text-secondary">ESL LOGO</h1>
+          <h1 className="font-bold text-text-secondary">Hoot ESL</h1>
           <p className="font-light text-text-secondary">By Matthew Campbell</p>
           <div className="flex gap-4 p-4 text-text-secondary [&>a]:hover:text-text-primary [&>a]:font-medium">
             <a href="">Games</a>
