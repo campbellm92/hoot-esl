8ae68988ba59dc183d84681846d17aea62dd1b9c
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 21:24:11 +0200

    move to new config dir

--- client/src/config/gamesConfig.js
+++ client/src/config/gamesConfig.js
@@ -1,5 +1,5 @@
-import testImg from "./assets/test.png";
-import wordPuzzlePopperImg from "./assets/word-puzzle-popper.webp";
+import testImg from "../assets/test.png";
+import wordPuzzlePopperImg from "../assets/word-puzzle-popper.webp";
 
 export const games = [
   {
@@ -8,8 +8,9 @@ export const games = [
       "Improve your knowledge of nouns, verbs and adjectives in this grouping game.",
     image: wordPuzzlePopperImg,
     languageFocus: "Parts of speech",
-    path: "./pages/games/word-puzzle-popper",
-    component: () => import("./pages/games/word-puzzle-popper/components/Game"),
+    path: "../pages/games/word-puzzle-popper",
+    component: () =>
+      import("../pages/games/word-puzzle-popper/components/Game"),
     link: "/games/word-puzzle-popper",
     config: {
       difficulty: {
@@ -31,8 +32,8 @@ export const games = [
       "Test your understanding of various words with this memory game",
     image: testImg,
     languageFocus: "Vocabulary",
-    path: "./pages/games/word-memory-game",
-    component: () => import("./pages/games/word-memory-game/components/Game"),
+    path: "../pages/games/word-memory-game",
+    component: () => import("../pages/games/word-memory-game/components/Game"),
     link: "/games/word-memory-game",
     config: {
       difficulty: {
