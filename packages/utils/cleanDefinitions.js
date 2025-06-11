9c41ad0e3a2138004dc8d80a6c044f45c87bdcf3
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 14:18:36 +0200

    revert to previous logic

--- packages/utils/cleanDefinitions.js
+++ packages/utils/cleanDefinitions.js
@@ -1,9 +1,4 @@
 // remove the markup tags in word definitions
 export default function cleanDefinition(text) {
-  return text
-    .replace(/\{.*?\}/g, ";") // replace tags with semicolon
-    .replace(/;{2,}/g, ";") // collapse multiple semicolons
-    .replace(/\s*;\s*/g, "; ") // normalise spaces around semicolons
-    .replace(/^;\s*/, "") // remove leading semicolon
-    .trim();
+  return text.replace(/\{.*?\}/g, "").trim();
 }
