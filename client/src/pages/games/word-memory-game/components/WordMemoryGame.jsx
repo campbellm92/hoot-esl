aaa65ff6caf19cf8746c6b648b03f4672329b7cc
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 15:31:49 +0200

    add logic and feedback for when game is completed

--- client/src/pages/games/word-memory-game/components/WordMemoryGame.jsx
+++ client/src/pages/games/word-memory-game/components/WordMemoryGame.jsx
@@ -3,14 +3,18 @@ import { useEffect, useState } from "react";
 import { useWordCards } from "../hooks/useWordCards.js";
 import MemoryCard from "./MemoryCard.jsx";
 import { Loading } from "@ui";
+import { Button } from "@ui";
 
 export default function WordMemoryGame() {
   const wordsPath = "/data/a1-words-POS.json";
-  const { cards, setCards, isLoading } = useWordCards(wordsPath); // contains card info from the hook, incl id, type, content, matchId
   const [turns, setTurns] = useState(0);
   const [cardOne, setCardOne] = useState(null);
   const [cardTwo, setCardTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [gameCompleted, setGameCompleted] = useState(false);
+  const [showGameCompleted, setShowGameCompleted] = useState(false);
+  const [resetGame, setResetGame] = useState(0);
+  const { cards, setCards, isLoading } = useWordCards(wordsPath, resetGame); // contains card info from the hook, incl id, type, content, matchId
 
   function handleChoice(card) {
     if (disabled) return;
@@ -23,6 +27,15 @@ export default function WordMemoryGame() {
     setCardTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
     setDisabled(false);
+  }
+
+  function handleResetGame() {
+    setCards([]);
+    setDisabled(false);
+    setGameCompleted(false);
+    setTurns(0);
+    setCards([]);
+    setResetGame((prev) => prev + 1);
   }
 
   useEffect(() => {
@@ -47,6 +60,36 @@ export default function WordMemoryGame() {
     }
   }, [cardOne, cardTwo]);
   console.log(cards);
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      const allMatched = cards.every((card) => card.matched);
+      if (allMatched && !gameCompleted) {
+        setGameCompleted(true);
+      }
+    }
+  }, [cards, gameCompleted]);
+
+  useEffect(() => {
+    if (gameCompleted && !showGameCompleted) {
+      setTimeout(() => {
+        setShowGameCompleted(true);
+      }, 1500);
+    }
+  }, [gameCompleted, showGameCompleted]);
+
+  if (gameCompleted && showGameCompleted) {
+    return (
+      <div className="flex flex-col justify-center items-center w-screen min-h-screen pb-96">
+        <h1 className="text-3xl text-text-primary font-semibold mb-6">
+          Game Completed!
+        </h1>
+        <p className="text-xl text-text-primary mb-8">Nice work!</p>
+        <p className="text-xl text-text-primary mb-8">Turns taken: {turns}</p>
+        <Button onClick={handleResetGame}>Play Again</Button>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
