5e7c44fe98035c8a635949f675f07e6458b47dbf
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 21:25:07 +0200

    import tools and tools grid

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from "../sections/home/Hero";
 import GamesGrid from "../sections/home/GamesGrid";
+import Tools from "../sections/home/Tools";
+import ToolsGrid from "../sections/home/ToolsGrid";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 justify-center items-center">
       <Hero />
       <GamesGrid />
+      <Tools />
+      <ToolsGrid />
     </div>
   );
 }
