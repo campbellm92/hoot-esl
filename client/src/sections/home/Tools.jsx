146377ac8c724d64a856a8a1fc4401b2c28281f3
Matthew Campbell <m.campbell92@gmail.com>
2025-06-11 21:25:48 +0200

    initial configuration of tools section

---
+++ client/src/sections/home/Tools.jsx
@@ -0,0 +1,19 @@
+// import { Button } from "@ui";
+
+export default function Tools() {
+  return (
+    <div className="flex flex-col justify-center items-center text-center w-10/12 h-auto p-4 border-4 border-text-primary rounded-lg">
+      <div className="mb-4">
+        <h2 className="font-semibold text-text-primary mb-4">
+          Tools for teachers
+        </h2>
+        <h3 className="text-text-primary mb-4">
+          <span className="font-semibold underline decoration-2 decoration-primary">
+            Teachers
+          </span>
+          : Discover useful tools to enhance your lesson planning.
+        </h3>
+      </div>
+    </div>
+  );
+}
